2061aad23c2479037d048a565339b19704658267
Leslie Ngwono Nyatigi <lesngwono84@gmail.com>
2022-09-20 16:45:45 +0300

    Completed assignment

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,27 @@
 import React from "react";
 import { name, city } from "../data/data.js";
 
+function ownerName(name) {
+  if (name) {
+    return "Liza";
+  }else {
+    return "Not Liza";
+  }
+}
+  function ownerCity(city) {
+    if(city) {
+      return "New York";
+    }else {
+      return "Nairobi"
+    }
+  }
+
 function Home() {
   // update the JSX being returned!
-  return <div>Home</div>;
+  return (
+  <div id="home">
+    <h1 style={{color:'firebrick'}}>{ownerName(name)} is a Web Developer from {ownerCity(city)}</h1>
+    </div>);
 }
 
 export default Home;
